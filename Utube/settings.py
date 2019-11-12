330f7b7e674d53ddc2c2c20e0ee617cbef4e23c4
mochatek <akashsabu@ymail.com>
2019-11-12 17:58:30 +0530

    debug set True

--- Utube/settings.py
+++ Utube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY','^d#23bwdu(tn3md5$%_=s12i!^i$fqyz88)y!2##t$)7yt#hq4')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
